8a9dae3b5deb9c6f4636adece6cfac82c5c2e5e8
GomRagzuv <gomezrafaelantonio@gmail.com>
2016-10-24 16:16:40 -0500

    Prueba github devKoding

--- src/devKoding/Test/RunTest.java
+++ src/devKoding/Test/RunTest.java
@@ -4,6 +4,8 @@ package devKoding.Test;
  * Created by cristhian on 24/10/16.
  */
 public class RunTest {
-
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
 
 }
