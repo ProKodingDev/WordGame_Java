1b3191c146066733fa04fc20d872257116bd65e0
GomRagzuv <gomezrafaelantonio@gmail.com>
2016-10-24 16:33:09 -0500

    Union Rama Test - master

--- src/devKoding/Test/RunTest.java
+++ src/devKoding/Test/RunTest.java
@@ -5,7 +5,7 @@ package devKoding.Test;
  */
 public class RunTest {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo2");
     }
 
 }
