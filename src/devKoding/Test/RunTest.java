b2c8c98f1da200fcb4d61bb25ada10fd533a6b77
cristhian <rdz.cristhian@gmail.com>
2016-10-24 16:26:06 -0500

    Librerias Sqlite conexion

--- src/devKoding/Test/RunTest.java
+++ src/devKoding/Test/RunTest.java
@@ -5,7 +5,7 @@ package devKoding.Test;
  */
 public class RunTest {
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola2");
     }
 
 }
