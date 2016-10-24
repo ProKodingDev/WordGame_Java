5ae77af5eb0c0114ea777c62218bf9570964d111
cristhian <rdz.cristhian@gmail.com>
2016-10-24 15:53:16 -0500

    Prueba Commit

---
+++ src/devKoding/logic/Run.java
@@ -0,0 +1,11 @@
+package devKoding.logic;
+
+/**
+ * Created by cristhian on 24/10/16.
+ */
+public class Run {
+
+    public static void main(String[] args) {
+        System.out.println("Prueba");
+    }
+}
